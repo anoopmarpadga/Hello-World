9adcfbd055a8400c33fef076c62cab04f14e9e19
anoopmarpadga <anoopmarpadga@gmail.com>
2017-04-02 09:22:16 +0530

    Add files via upload

---
+++ Primenumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Primenumber
+    {
+        public static void Main(String []args)
+        {
+            int num,i;
+            Console.WriteLine("ENTER THE NUMBER TO CHECK IF PRIME OR NOT");
+            num=int.Parse(Console.ReadLine());
+            for (i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("Not a prime number");
+                    break;
+                }
+
+            } 
+         if(i==num)
+            {
+                Console.WriteLine("prime number");
+            }
+            Console.Read();
+        }
+        }
+
+    }
+
---
+++ factorial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int i, f=1;
+
+            if (n <= 0)
+
+            {
+
+                Console.WriteLine("Factorial cannot be determined");
+            }
+
+            else
+            {
+                for (i = 1; i <= n; i++)
+                {
+                    f = f * i;
+                }
+                Console.WriteLine(f);
+                Console.Read();
+            }
+            Console.Read();
+        }
+
+    }
+}
