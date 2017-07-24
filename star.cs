e6ee29b23a45e1a6d216f554331cba9993a04b59
ANOOP REDDY <anoopmarpadga@gmail.com>
2017-07-24 21:16:51 +0530

    Star program to print stars in increasing order

---
+++ star.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.WriteLine("Enter the number till where the * series to be printed");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number > 0)
+            {
+                Console.WriteLine("The Output is");
+                for (int i = 0; i < number; i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        Console.Write("*");
+
+                    }
+                    Console.WriteLine();
+                }
+
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
+
